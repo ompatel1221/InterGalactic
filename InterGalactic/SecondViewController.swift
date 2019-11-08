15d1d9e1e10966c37538a66a21ebc4aaf7799090
Om Patel <opatel0464@stu.d214.org>
2019-11-08 11:27:00 -0600

    created iboutlet for second image view

--- InterGalactic/SecondViewController.swift
+++ InterGalactic/SecondViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var starImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
